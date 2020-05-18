d4407a3502ced1524218daaaa9d09c9f3aa86b72
sandip95 <60126846+sandip95@users.noreply.github.com>
2020-05-18 11:42:15 +0530

    Add files via upload

---
+++ Javateditor.java
@@ -0,0 +1,16 @@
+
+package javateditor;
+
+
+public class Javateditor {
+
+    
+    public static void main(String[] args) 
+    {
+      texteditor.Jframe eframe = new texteditor.Jframe();
+      eframe.setLocationRelativeTo(null);
+      eframe.setDefaultCloseOperation(texteditor.Jframe.EXIT_ON_CLOSE);
+      eframe.setVisible(true);
+    }
+    
+}
